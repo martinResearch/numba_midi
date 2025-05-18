26ca6c4a15a556bd38f651a5ac787fcbfe3be495
Martin de La Gorce <madelago@microsoft.com>
2025-05-18 16:46:57 +0100

    run test before publishing

--- tests/test_score.py
+++ tests/test_score.py
@@ -25,12 +25,13 @@ def test_get_beat_positions() -> None:
         print(f"Testing get_beat_positions with {midi_file}")
         # load row midi score
         score = load_score(midi_file)
-        beat_positions, bar = score.get_beat_and_bar_times()
-        assert len(beat_positions) > 0
-        assert np.all(np.diff(beat_positions) > 0)
-
-        assert len(bar) > 0
-        assert np.all(np.diff(bar) > 0)
+        subdivisions,  beats, bars = score.get_subdivision_beat_and_bar_times()
+        assert len(subdivisions) > 0
+        assert np.all(np.diff(subdivisions) > 0)
+        assert len(beats) > 0
+        assert np.all(np.diff(beats) > 0)
+        assert len(bars) > 0
+        assert np.all(np.diff(bars) > 0)
 
 
 def test_signature() -> None:
@@ -75,23 +76,24 @@ def test_signature() -> None:
         score.add_track(track)
 
         print(score)
-        beat, bar = score.get_beat_and_bar_times()
-        print("Beat positions:", beat)
-        print("Bar positions:", bar)
-        assert len(beat) > 0
-        assert len(bar) > 0
+        subdivisions, beats, bars = score.get_subdivision_beat_and_bar_times()
+        print("Beat positions:", beats)
+        print("Bar positions:", bars)
+        assert len(beats) > 0
+        assert len(bars) > 0
 
         # calculating the musical BPM i.e number of beat per minute and
         # taking the signature denominator into account
         # While people often confusingly refer to the number of quarter
         # notes per minute as the BPM
+
         expected_bpm = get_bpm_from_quarter_notes_per_minute(quarter_notes_per_minute, numerator, denominator)
-        bpm = 60.0 / (beat[1] - beat[0])
+        bpm = 60.0 / (beats[1] - beats[0])
         assert np.isclose(bpm, expected_bpm), f"Expected {expected_bpm}, got {bpm}"
 
         expected_bar_duration = get_bar_duration(quarter_notes_per_minute, numerator, denominator)
-        assert np.isclose(bar[1] - bar[0], expected_bar_duration), (
-            f"Expected {expected_bar_duration}, got {bar[1] - bar[0]}"
+        assert np.isclose(bars[1] - bars[0], expected_bar_duration), (
+            f"Expected {expected_bar_duration}, got {bars[1] - bars[0]}"
         )
 
 
